22b28debc1110407f2e12dca16ed769f8a2419ce
Vigneshwar Ravichandran <vigneshwarravichandran@gmail.com>
2019-08-06 23:47:11 +0530

    Added day4 permutations
    Signed-off-by: Vigneshwar Ravichandran <vigneshwarravichandran@gmail.com>

--- day6_permutation_combination.py
+++ day6_permutation_combination.py
@@ -1,3 +1,5 @@
+from itertools import permutations
+
 def permutation(string, dict):
 	dict[string] = []
 	for itr in range(len(string)):
@@ -7,13 +9,11 @@ def permutation(string, dict):
 			result = permutation(value, dict)
 	return dict
 
-def combination(string):
-	pass
-
 string = '459'
 dict = {}
 result_permutation = permutation(string, dict)
 result_permutation = result_permutation.keys()
 result_permutation.sort(key=len, reverse=True)
 print(result_permutation)
-result_combination = combination(string)
+result_combination = list(permutations(list(string)))
+print(result_combination)
