8092cca497a0104150c5cba1ed64431d1562db9e
Vigneshwar Ravichandran <vigneshwarravichandran@gmail.com>
2019-08-11 23:28:06 +0530

    Simple graph implementation
    Signed-off-by: Vigneshwar Ravichandran <vigneshwarravichandran@gmail.com>

---
+++ day11_graph.py
@@ -0,0 +1,19 @@
+def find_path(graph, start, end, path): 
+	path.append(start) 
+	if start == end: 
+		return path 
+	for node in graph[start]: 
+		if node not in path: 
+			newpath = find_path(graph, node, end, path) 
+		if newpath: 
+			return newpath 
+		return None
+
+graph = { 
+	'a' : ['c'], 
+	'b' : ['d'], 
+	'c' : ['e'], 
+	'd' : ['a', 'd'], 
+	'e' : ['b', 'c'] 
+} 
+print(find_path(graph, 'd', 'c', [])) 
