41e7ee60b6b01af250c1ff9cf4ac474957c7af07
Vigneshwar Ravichandran <vigneshwarravichandran@gmail.com>
2019-08-15 00:26:12 +0530

    Queue implementation
    Signed-off-by: Vigneshwar Ravichandran <vigneshwarravichandran@gmail.com>

--- day13_queue.py
+++ day13_queue.py
@@ -1,1 +1,20 @@
-queue = []
+class Queue:
+	def __init__(self):
+		self.queue = []
+
+	def push(self, value):
+		self.queue.append(value)
+		self.display_queue()
+
+	def pop(self):
+		self.queue.pop(0)
+		self.display_queue()
+
+	def display_queue(self):
+		print(self.queue)
+
+queue = Queue()
+queue.push(1)
+queue.push(3)
+queue.push(4)
+queue.pop()
